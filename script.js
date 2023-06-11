cc337ae522ed5b79f08263532ea21319401577f1
ganeshnithya185 <133205566+ganeshnithya185@users.noreply.github.com>
2023-06-11 15:01:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+const names = ['Mani Ram', 'Binledan', 'Tom']
+
+function getThingOne (callback1, callback2) {
+    //call getThingTwo and pass a name and callback (getThingThree)
+   return callback1([names[0]], callback2)
+}
+
+function getThingTwo (list, callback) {
+  //call getThingThree, and concat the list with the newest name
+  return callback([...list, names[1]])
+}
+
+function getThingThree (list) {
+  //return all the names
+  return `${list},${names[2]}`;
+}
+
+let result = document.getElementById('result');
+result.innerText = getThingOne(getThingTwo, getThingThree);
+
